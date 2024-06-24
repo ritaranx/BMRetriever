ad783bd4dcd3fabd847518ced775423890c97ca5
Ran Xu <47405113+ritaranx@users.noreply.github.com>
2024-06-24 16:39:30 -0700

    Add files via upload

---
+++ prompt.py
@@ -0,0 +1,19 @@
+
+
+task_name_to_instruct = {
+    "gooaq": "Given a question, retrieve relevant passages that answer the question",
+    "eli5": "Provided a user question, retrieve the highest voted answers on Reddit ELI5 forum",
+    "squad": "Given a question, retrieve Wikipedia passages that answer the question",
+    "nli": "Given a premise, retrieve a hypothesis that is entailed by the premise", 
+    'msmarco': 'Given a web search query, retrieve relevant passages that answer the query',
+    'nq': 'Given a question, retrieve Wikipedia passages that answer the question',
+    'quora': 'Given a question, retrieve questions that are semantically equivalent to the given question',
+    "fever": 'Given a claim, retrieve documents that support or refute the claim',
+    "hotpotqa": 'Given a multi-hop question, retrieve documents that can help answer the question',
+    "msmarco_doc": "Given a web search query, retrieve relevant documents that answer the query",
+    'nfcorpus': 'Given a question, retrieve relevant documents that best answer the question',
+    'scidocs': 'Given a scientific paper title, retrieve paper abstracts that are cited by the given paper',
+    'scifact': 'Given a scientific claim, retrieve documents that support or refute the claim',
+    'trec-covid': 'Given a query on COVID-19, retrieve documents that answer the query',
+   }
+    
